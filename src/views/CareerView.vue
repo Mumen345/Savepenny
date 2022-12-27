2b32496fbb151682b94fc4fd8eddc1cc477b0102
Elijah <kingxosas@gmail.com>
2022-12-27 03:18:05 +0100

    done with my thing

--- src/views/CareerView.vue
+++ src/views/CareerView.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-
+        <CareerHero />
+        <FindJobs />
+        <FooterSection />
     </div>
 </template>
 
 <script>
-export default {
+import CareerHero from '@/components/CareerHero.vue';
+import FindJobs from '@/components/FindJobs.vue';
+import FooterSection from '@/components/FooterSection.vue';
 
+export default {
+    components: { CareerHero, FooterSection, FindJobs }
 }
 </script>
 
